261e16733116d8114252c30603d6384714efc440
nwagelmans <nick@cg.nl>
2018-03-23 11:35:58 +0100

    Add Computer Vision OCR to Voucher.

--- app/models/voucher.rb
+++ app/models/voucher.rb
@@ -10,8 +10,7 @@ class Voucher < ApplicationRecord
   def process_image
     process_image_by_google
     process_image_by_tesseract
-    # process_image_by_ocr_space
-    # process_image_by_computer_vision
+    process_image_by_computer_vision
     save
   end
 
@@ -35,5 +34,28 @@ class Voucher < ApplicationRecord
     resource = OcrSpace::Resource.new(apikey: ENV['OCR_API_KEY'])
     result = resource.convert file: image.current_path
     self.code_ocr_space = result.try(:[], 'ParsedText')
+  end
+
+  def process_image_by_computer_vision
+    require 'net/http'
+
+    uri = URI('https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/ocr')
+    uri.query = URI.encode_www_form({
+      # 'detectOrientation' => 'true',
+      # 'language' => 'en'
+    })
+
+    request = Net::HTTP::Post.new(uri.request_uri)
+    request['Content-Type'] = 'application/octet-stream'
+    request['Ocp-Apim-Subscription-Key'] = '86c29ff3270e4503bfa6769f478045dd'
+    request.body = File.new(image.current_path, 'rb').read
+
+    response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+      http.request(request)
+    end
+
+    if response.code == '200'
+      self.code_computer_vision = response.body.scan(/"text":"(.+?)"/).join(" ")
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180321142532) do
+ActiveRecord::Schema.define(version: 20180323112741) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20180321142532) do
     t.string "code_google"
     t.string "code_tesseract"
     t.string "code_ocr_space"
+    t.string "code_computer_vision"
   end
 
 end
